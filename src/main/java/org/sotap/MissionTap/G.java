e140750a6e7793c987b0b25688502896b220eded
Subilan <suyuanliang1995@gmail.com>
2020-07-29 20:55:55 +0800

    Add 'G' helper class

---
+++ src/main/java/org/sotap/MissionTap/G.java
@@ -0,0 +1,20 @@
+package org.sotap.MissionTap;
+
+import org.bukkit.ChatColor;
+
+public final class G {
+    public final static String SUCCESS = "&r[&aSUCCESS&r] ";
+    public final static String WARN = "&r[&eWARN&r] ";
+    public final static String FAILED = "&r[&cFAILED&r] ";
+    public final static String INFO = "&r[&bINFO&r] ";
+
+    /**
+     * 调用 ChatColor 对 '&' 进行转义
+     * 
+     * @param message 要转义的字符串
+     * @return 转义后的字符串
+     */
+    public static String translateColor(String message) {
+        return ChatColor.translateAlternateColorCodes('&', message);
+    }
+}
