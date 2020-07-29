71f919db8b580b62396bc349b1553a3977d0c536
Subilan <suyuanliang1995@gmail.com>
2020-07-30 00:23:24 +0800

    Run Timer as async self-cancel bukkit task

--- src/main/java/org/sotap/MissionTap/MissionTap.java
+++ src/main/java/org/sotap/MissionTap/MissionTap.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
 
 public final class MissionTap extends JavaPlugin {
     public FileConfiguration specialMissions;
@@ -23,6 +24,7 @@ public final class MissionTap extends JavaPlugin {
         this.dailyMissions = load("daily-missions.yml");
         this.weeklyMissions = load("weekly-missions.yml");
         this.latestMissions = load("latest-missions.yml");
+        BukkitTask timer = new Timer(this).runTaskTimer(this, 0, 20);
         log(G.translateColor(G.SUCCESS + "The plugin has been &aenabled&r."));
     }
 
@@ -49,5 +51,6 @@ public final class MissionTap extends JavaPlugin {
 
     /**
      * TODO: Random map or other possible data format.
+     * Please make sure writing a expiration time at the beginning of the latest-missions.yml.
      */
 }
