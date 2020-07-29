eb0834ef7015eb624be056a59de7c497a9e23c42
Subilan <suyuanliang1995@gmail.com>
2020-07-29 20:56:09 +0800

    Base codes of a plugin

---
+++ src/main/java/org/sotap/MissionTap/MissionTap.java
@@ -0,0 +1,51 @@
+package org.sotap.MissionTap;
+
+import java.io.File;
+import java.io.IOException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MissionTap extends JavaPlugin {
+    public FileConfiguration constantMissions;
+    public FileConfiguration dailyMissions;
+    public FileConfiguration weeklyMissions;
+
+    public void log(String message) {
+        getLogger().info(message);
+    }
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        reloadMissions();
+        log(G.translateColor(G.SUCCESS + "The plugin has been &aenabled&r."));
+    }
+
+    @Override
+    public void onDisable() {
+        log(G.translateColor(G.SUCCESS + "The plugin has been &cdisabled&r."));
+    }
+
+    public FileConfiguration load(String filename) {
+        File folder = getDataFolder();
+        File file = new File(folder, filename);
+        if (!folder.exists()) {
+            folder.mkdir();
+        }
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return YamlConfiguration.loadConfiguration(file);
+    }
+
+    public void reloadMissions() {
+        constantMissions = load("constant-missions.yml");
+        dailyMissions = load("daily-missions.yml");
+        weeklyMissions = load("weekly-missions.yml");
+    }
+}
