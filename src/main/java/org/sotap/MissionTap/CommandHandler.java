af661e982b6558302949f9ce93ad2e7b49416ece
Subilan <suyuanliang1995@gmail.com>
2020-07-30 20:39:45 +0800

    Commands to open the menus (for testing)

---
+++ src/main/java/org/sotap/MissionTap/CommandHandler.java
@@ -0,0 +1,35 @@
+package org.sotap.MissionTap;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+
+public final class CommandHandler implements CommandExecutor {
+    public MissionTap plug;
+
+    public CommandHandler(MissionTap plug) {
+        this.plug = plug;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("missiontap")) {
+            Player senderPlayer = Bukkit.getPlayer(sender.getName());
+            switch (args[0]) {
+                case "daily": {
+                    plug.dailyMissionGUI.open(senderPlayer);
+                    break;
+                }
+
+                case "weekly": {
+                    plug.weeklyMissionGUI.open(senderPlayer);
+                    break;
+                }
+            }
+        }
+        return false;
+    }
+}
