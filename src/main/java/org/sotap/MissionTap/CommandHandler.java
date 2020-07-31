4d27affe3196cd4a1244811f9611ee0460df74fc
Subilan <suyuanliang1995@gmail.com>
2020-07-31 17:26:42 +0800

    Remove unused import (small)

--- src/main/java/org/sotap/MissionTap/CommandHandler.java
+++ src/main/java/org/sotap/MissionTap/CommandHandler.java
@@ -4,7 +4,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
 public final class CommandHandler implements CommandExecutor {
