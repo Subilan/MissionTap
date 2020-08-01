45255ad2b1e41008d12cd7b91534a2e4de8dae6d
Subilan <suyuanliang1995@gmail.com>
2020-08-01 18:16:56 +0800

    Class Acceptance

---
+++ src/main/java/org/sotap/MissionTap/Acceptance.java
@@ -0,0 +1,29 @@
+package org.sotap.MissionTap;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.sotap.MissionTap.Utils.G;
+
+public final class Acceptance {
+    public Mission proto;
+    public String key;
+
+    public Acceptance(Mission m) {
+        this.proto = m;
+        this.key = m.key;    
+    }
+
+    public Map<String,Object> getAcceptance() {
+        Map<String,Object> acc = new HashMap<>();
+        FileConfiguration missions = YamlConfiguration.loadConfiguration(G.loadFile(null, "latest-missions.yml"));
+        Long nextUpdateTime = missions.getLong(proto.type + "-next-regen");
+        acc.put("name", proto.name);
+        acc.put("acceptance-time", new Date().getTime());
+        acc.put("expiration-time", nextUpdateTime);
+        acc.put("finished", false);
+        return acc;
+    }
+}
