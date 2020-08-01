dc09c9bc584ca4915fb0abb621995ec80a5da17b
Subilan <suyuanliang1995@gmail.com>
2020-08-01 20:47:59 +0800

    Get data when file configuration arg is not null

--- src/main/java/org/sotap/MissionTap/Acceptance.java
+++ src/main/java/org/sotap/MissionTap/Acceptance.java
@@ -3,23 +3,38 @@ package org.sotap.MissionTap;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.sotap.MissionTap.Utils.G;
 
 public final class Acceptance {
-    public Mission proto;
     public String key;
+    public String type;
+    public String name;
+    public boolean finished;
+    public long acceptanceTime;
+    public long expirationTime;
+    private ConfigurationSection acc;
 
-    public Acceptance(Mission m) {
-        this.proto = m;
-        this.key = m.key;    
+    public Acceptance(String key, FileConfiguration data, String type, String name) {
+        this.key = key;
+        this.type = type;
+        if (data != null) {
+            this.acc = data.getConfigurationSection(type + "." + key);
+            this.name = acc.getString("name");
+            this.finished = acc.getBoolean("finished");
+            this.acceptanceTime = acc.getLong("accpetance-time");
+            this.expirationTime = acc.getLong("expiration-time");
+        } else {
+            this.name = name;
+        }
     }
 
     public Map<String,Object> getAcceptance() {
         Map<String,Object> acc = new HashMap<>();
         FileConfiguration missions = G.load("latest-missions.yml");
-        Long nextUpdateTime = missions.getLong(proto.type + "-next-regen");
-        acc.put("name", proto.name);
+        Long nextUpdateTime = missions.getLong(type + "-next-regen");
+        acc.put("name", name);
         acc.put("acceptance-time", new Date().getTime());
         acc.put("expiration-time", nextUpdateTime);
         acc.put("finished", false);
