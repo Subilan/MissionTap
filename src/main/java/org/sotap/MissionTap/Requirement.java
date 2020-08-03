74d455ddef32eab0f15a6281259a00afe0957b17
Subilan <suyuanliang1995@gmail.com>
2020-08-04 02:02:28 +0800

    Allow specific empty requirement

--- src/main/java/org/sotap/MissionTap/Requirement.java
+++ src/main/java/org/sotap/MissionTap/Requirement.java
@@ -14,11 +14,15 @@ public final class Requirement {
     public Requirement(String type, String key, String compareType, Map<String, Object> toCompare) {
         this.toCompare = cast(toCompare);
         this.compareType = compareType;
-        this.contents = G.load("latest-missions.yml").getConfigurationSection(type + "." + key + ".contents");
+        this.contents = G.load("latest-missions.yml")
+                .getConfigurationSection(type + "." + key + ".contents");
     }
 
     public boolean met() {
-        Map<String, Integer> compare = cast(contents.getConfigurationSection(compareType).getValues(false));
+        ConfigurationSection actualContents = contents.getConfigurationSection(compareType);
+        if (actualContents == null)
+            return true;
+        Map<String, Integer> compare = cast(actualContents.getValues(false));
         for (String k : compare.keySet()) {
             if (compare.get(k) > toCompare.get(k))
                 return false;
@@ -27,13 +31,13 @@ public final class Requirement {
     }
 
     /**
-     * 强行让编译器将 Map<String,Object> 作为 Map<String,Integer> 来处理
-     * 这种方法有效解决了性能上的无谓损失，因为已经确定了其所有值必为 Integer
+     * 强行让编译器将 Map(String,Object) 作为 Map(String,Integer) 来处理 这种方法有效解决了性能上的无谓损失，因为已经确定了其所有值必为 Integer
      * 若通过循环来创建一个新的 Map 则显得没有必要。因此通过这种欺骗编译器的方式来解决。
-     * @param object 要强行识别的 Map<String,Object>
-     * @return Map<String,Integer>
+     * 
+     * @param object 要强行识别的 Map(String,Object)
+     * @return Map(String,Integer)
      */
-    public Map<String,Integer> cast(Map<String,Object> object) {
+    public Map<String, Integer> cast(Map<String, Object> object) {
         @SuppressWarnings({"unchecked", "rawtypes"})
         Map<String, Integer> hack = (Map) object;
         return hack;
