071b201ebb792cd9282ae1bb14dd9e8e3fb74442
Subilan <suyuanliang1995@gmail.com>
2020-08-04 01:44:44 +0800

    uSe tHE hAcK wAy tO sOLVe tHis

--- src/main/java/org/sotap/MissionTap/Requirement.java
+++ src/main/java/org/sotap/MissionTap/Requirement.java
@@ -1,8 +1,6 @@
 package org.sotap.MissionTap;
 
 import java.util.Map;
-import java.util.stream.Collectors;
-
 import org.bukkit.configuration.ConfigurationSection;
 import org.sotap.MissionTap.Utils.G;
 
@@ -26,8 +24,16 @@ public final class Requirement {
         return true;
     }
 
+    /**
+     * 强行让编译器将 Map<String,Object> 作为 Map<String,Integer> 来处理
+     * 这种方法有效解决了性能上的无谓损失，因为已经确定了其所有值必为 Integer
+     * 若通过循环来创建一个新的 Map 则显得没有必要。因此通过这种欺骗编译器的方式来解决。
+     * @param object 要强行识别的 Map<String,Object>
+     * @return Map<String,Integer>
+     */
     public Map<String,Integer> cast(Map<String,Object> object) {
-        return object.entrySet().stream()
-        .collect(Collectors.toMap(Map.Entry::getKey, e -> (Integer) e.getValue()));
+        @SuppressWarnings({"unchecked", "rawtypes"})
+        Map<String, Integer> hack = (Map) object;
+        return hack;
     }
 }
