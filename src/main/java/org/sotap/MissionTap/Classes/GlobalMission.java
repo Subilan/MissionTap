fa5ba7740b96240a787a1431a3aa0a0ad5c496b5
Subilan <suyuanliang1995@gmail.com>
2020-08-09 01:05:45 +0800

    Clear mission by expiration info before global acc

--- src/main/java/org/sotap/MissionTap/Classes/GlobalMission.java
+++ src/main/java/org/sotap/MissionTap/Classes/GlobalMission.java
@@ -3,6 +3,7 @@ package org.sotap.MissionTap.Classes;
 import java.util.List;
 import java.util.UUID;
 import org.bukkit.configuration.ConfigurationSection;
+import org.sotap.MissionTap.Utils.Calendars;
 import org.sotap.MissionTap.Utils.Files;
 
 public final class GlobalMission {
@@ -18,9 +19,19 @@ public final class GlobalMission {
         List<UUID> uuids = Files.getAllPlayerUUID();
         for (String key : missions.getValues(false).keySet()) {
             for (UUID u : uuids) {
+                clearExpiredMissions(u);
                 Mission m = new Mission(type, key);
                 m.accept(u);
             }
+        }
+    }
+
+    public void clearExpiredMissions(UUID u) {
+        ConfigurationSection section = Files.loadPlayer(u).getConfigurationSection(type);
+        if (Files.isEmptyConfiguration(section)) return;
+        for (String key : section.getValues(false).keySet()) {
+            if (section.getConfigurationSection(key).getLong("expiration") > Calendars.getNow()) continue;
+            section.set(key, null);
         }
     }
 }
