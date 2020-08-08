64e28aae8523f4726414aacd9553fc808a7cc816
Subilan <suyuanliang1995@gmail.com>
2020-08-09 01:04:29 +0800

    Add method for getting exact expiration

--- src/main/java/org/sotap/MissionTap/Utils/Calendars.java
+++ src/main/java/org/sotap/MissionTap/Utils/Calendars.java
@@ -6,7 +6,7 @@ import java.util.Date;
 import java.util.List;
 
 public final class Calendars {
-    
+
     public static SimpleDateFormat getFormatter() {
         return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     }
@@ -43,15 +43,33 @@ public final class Calendars {
         return cal.getTime();
     }
 
+    public static long getMissionExpiration(String type) {
+        if (!List.of("daily", "weekly").contains(type))
+            return 0;
+        if (type == "daily") {
+            return getNextDailyRefresh().getTime();
+        } else {
+            Date weeklyRefresh = getNextWeeklyRefresh();
+            if (Files.config.getBoolean("allow-tarriance")) {
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(new Date());
+                cal.add(Calendar.MONTH, 1);
+                weeklyRefresh = cal.getTime();
+            }
+            return weeklyRefresh.getTime();
+        }
+    }
+
     public static long getNextRefresh(String type) {
-        if (!List.of("daily", "weekly").contains(type)) return 0;
+        if (!List.of("daily", "weekly").contains(type))
+            return 0;
         return type == "daily" ? getNextDailyRefresh().getTime() : getNextWeeklyRefresh().getTime();
     }
 
     // public static boolean isNowTheFirstWeekOfTheMonth() {
-    //     Calendar cal = Calendar.getInstance();
-    //     cal.setTime(new Date());
-    //     Integer now = cal.get(Calendar.WEEK_OF_MONTH);
-    //     return now == 1;
+    // Calendar cal = Calendar.getInstance();
+    // cal.setTime(new Date());
+    // Integer now = cal.get(Calendar.WEEK_OF_MONTH);
+    // return now == 1;
     // }
 }
