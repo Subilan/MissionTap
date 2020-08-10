67621ff0bd464720e6b0642f94ec44b48d85dc25
Subilan <suyuanliang1995@gmail.com>
2020-08-10 15:02:24 +0800

    Only show full cmds that player has permission to

--- src/main/java/org/sotap/MissionTap/Commands/Tab.java
+++ src/main/java/org/sotap/MissionTap/Commands/Tab.java
@@ -6,21 +6,29 @@ import java.util.List;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 import org.bukkit.util.StringUtil;
 
 public final class Tab implements TabCompleter {
-    private final List<String> ITERABLE_BASE;
+    private static final String[] BASE = {"daily", "weekly", "inprogress", "special", "about", "init", "player", "reload"};
 
-    public Tab() {
-        this.ITERABLE_BASE = List.of("daily", "weekly", "inprogress", "special", "d", "w", "i", "s",
-                "about", "init", "player", "reload");
+    public Tab() {}
+
+    public List<String> getAvailableCommands(Player p) {
+        List<String> available = new ArrayList<>();
+        for (String cmd : BASE) {
+            if (p.hasPermission("missiontap." + cmd)) {
+                available.add(cmd);
+            }
+        }
+        return available;
     }
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] arguments) {
         List<String> result = new ArrayList<>();
         if (command.getName().equalsIgnoreCase("missiontap")) {
-            result = StringUtil.copyPartialMatches(arguments[0], ITERABLE_BASE, result);
+            result = StringUtil.copyPartialMatches(arguments[0], getAvailableCommands((Player) sender), result);
             Collections.sort(result);
         }
         return result;
