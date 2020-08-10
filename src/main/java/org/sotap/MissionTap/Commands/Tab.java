a2f414d2a6f46917d694635e93cd533a3b581565
Subilan <suyuanliang1995@gmail.com>
2020-08-10 23:53:57 +0800

    Add complete for new command arguments

--- src/main/java/org/sotap/MissionTap/Commands/Tab.java
+++ src/main/java/org/sotap/MissionTap/Commands/Tab.java
@@ -1,6 +1,7 @@
 package org.sotap.MissionTap.Commands;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import org.bukkit.command.Command;
@@ -10,9 +11,13 @@ import org.bukkit.entity.Player;
 import org.bukkit.util.StringUtil;
 
 public final class Tab implements TabCompleter {
-    private static final String[] BASE = {"daily", "weekly", "inprogress", "special", "about", "init", "player", "reload"};
+    private static final String[] BASE = { "daily", "weekly", "inprogress", "special", "about", "init", "player",
+            "reload", "enable", "disable" };
+    private static final String[] PLAYER_OPTIONS = {"clear-submittions", "clear-missions"};
+    private static final String[] ENABLE_DISABLE_OPTIONS = {"special"};
 
-    public Tab() {}
+    public Tab() {
+    }
 
     public List<String> getAvailableCommands(Player p) {
         List<String> available = new ArrayList<>();
@@ -24,15 +29,40 @@ public final class Tab implements TabCompleter {
         return available;
     }
 
+    public List<String> getResult(String arg, List<String> commands) {
+        List<String> result = new ArrayList<>();
+        result = StringUtil.copyPartialMatches(arg, commands, result);
+        Collections.sort(result);
+        return result;
+    }
+
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] arguments) {
         List<String> result = new ArrayList<>();
         if (command.getName().equalsIgnoreCase("missiontap")) {
-            if (arguments.length == 1) {
-                result = StringUtil.copyPartialMatches(arguments[0], getAvailableCommands((Player) sender), result);
-                Collections.sort(result);
-            } else {
-                result = null;
+            switch (arguments.length) {
+                case 1: {
+                    result = getResult(arguments[0], getAvailableCommands((Player) sender));
+                    break;
+                }
+
+                case 2: {
+                    if (arguments[0] == "enable" || arguments[0] == "disable") {
+                        result = getResult(arguments[1], Arrays.asList(ENABLE_DISABLE_OPTIONS));
+                    }
+                    break;
+                }
+
+                case 3: {
+                    if (arguments[0] == "player") {
+                        result = getResult(arguments[0], Arrays.asList(PLAYER_OPTIONS));
+                    }
+                    break;
+                }
+
+                default: {
+                    result = null;
+                }
             }
         }
         return result;
