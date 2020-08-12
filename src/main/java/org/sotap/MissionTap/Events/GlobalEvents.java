9b415825932c9850efc7eb1556f1c9b6d70c746d
Subilan <suyuanliang1995@gmail.com>
2020-08-12 23:22:36 +0800

    Call init player function

--- src/main/java/org/sotap/MissionTap/Events/GlobalEvents.java
+++ src/main/java/org/sotap/MissionTap/Events/GlobalEvents.java
@@ -1,12 +1,12 @@
 package org.sotap.MissionTap.Events;
 
-import java.util.UUID;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.sotap.MissionTap.MissionTap;
+import org.sotap.MissionTap.Utils.Functions;
 
 public final class GlobalEvents implements Listener {
     public MissionTap plugin;
@@ -19,7 +19,6 @@ public final class GlobalEvents implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
         Player p = e.getPlayer();
-        UUID u = p.getUniqueId();
-        // big deal here
+        Functions.initPlayer(p);
     }
 }
