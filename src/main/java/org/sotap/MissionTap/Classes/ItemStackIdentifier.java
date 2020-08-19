b6c7236b1726af133283fd5fa3c0f911306e05c4
Subilan <suyuanliang1995@gmail.com>
2020-08-20 02:41:45 +0800

    New class ItemSTackIdentifier

---
+++ src/main/java/org/sotap/MissionTap/Classes/ItemStackIdentifier.java
@@ -0,0 +1,46 @@
+package org.sotap.MissionTap.Classes;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
+import org.bukkit.inventory.ItemStack;
+import org.sotap.MissionTap.MissionTap;
+
+public final class ItemStackIdentifier {
+    public Map<UUID, Map<ItemStack, Boolean>> identified;
+
+    public ItemStackIdentifier(MissionTap plugin) {
+        this.identified = new HashMap<>();
+    }
+
+    public void addIdentifier(ItemStack stack) {
+        UUID random = UUID.randomUUID();
+        identified.put(random, createState(stack, true));
+    }
+
+    public boolean isIdentified(ItemStack stack, boolean requireValid) {
+        return identified.containsValue(createState(stack, requireValid));
+    }
+
+    public boolean isIdentified(ItemStack stack) {
+        return identified.containsValue(createState(stack, true))
+                || identified.containsValue(createState(stack, false));
+    }
+
+    public void setInvalid(ItemStack stack) {
+        for (UUID u : identified.keySet()) {
+            if (Objects.equals(identified.get(u), createState(stack, true))) {
+                identified.remove(u);
+                identified.put(u, createState(stack, false));
+                break;
+            }
+        }
+    }
+
+    private Map<ItemStack, Boolean> createState(ItemStack stack, Boolean bool) {
+        Map<ItemStack, Boolean> map = new HashMap<>();
+        map.put(stack, bool);
+        return map;
+    }
+}
