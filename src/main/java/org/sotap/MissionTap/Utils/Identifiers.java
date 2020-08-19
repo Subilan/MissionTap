fd7b373ca56f11cabb8292b3461e7ab14984282b
Subilan <suyuanliang1995@gmail.com>
2020-08-20 02:49:13 +0800

    Add javadoc

--- src/main/java/org/sotap/MissionTap/Utils/Identifiers.java
+++ src/main/java/org/sotap/MissionTap/Utils/Identifiers.java
@@ -11,18 +11,38 @@ public final class Identifiers {
         identifier = new ItemStackIdentifier(plugin);
     }
 
+    /**
+     * 为指定 stack 加上 UUID 标识
+     * @param stack 指定 stack
+     */
     public static void identify(ItemStack stack) {
         identifier.addIdentifier(stack);
     }
-
+    
+    /**
+     * 判断 stack 是否包含有效标识
+     * 
+     * @param stack
+     * @return 是否包含有效标识
+     */
     public static boolean isValidIdentified(ItemStack stack) {
         return identifier.isIdentified(stack, true);
     }
     
-    public static boolean isIdentified(ItemStack stack) {
+    /**
+     * 判断 stack 是否包含无效标识
+     * 
+     * @param stack
+     * @return 是否包含无效标识
+     */
+    public static boolean isInvalidIdentified(ItemStack stack) {
         return identifier.isIdentified(stack, false);
     }
 
+    /**
+     * 标记 stack 的标识为无效
+     * @param stack
+     */
     public static void setInvalid(ItemStack stack) {
         identifier.setInvalid(stack);
     }
